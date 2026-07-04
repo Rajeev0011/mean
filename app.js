a8da58c066e8bf2f67303e5ab9bdb40f61ac9148
Rajeev0011 <rajeevsinghmca01@gmail.com>
2026-07-04 13:23:23 +0530

    added model and route

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 
 const PORT = '3001';
+
+const dns = require('node:dns/promises');
+
+dns.setServers(['1.1.1.1', '1.0.0.1']);
+
+//enable cors
+app.use(cors());
+//enable json payload
+app.use(express.json());
+
+//route the customer api
+const customerRoutes = require('../backend/routes/customers');
+//use the routes
+app.use('/api/customers', customerRoutes);
 
 app.get('/', (req, res) => {
   res.send('Welcome to Customer Api');
@@ -13,3 +29,12 @@ app.listen(PORT, (error) => {
     console.log(`Server Error ${error}`);
   }
 });
+
+connectDB().catch((error) => console.error(error));
+
+async function connectDB() {
+  const connectionString =
+    'mongodb+srv://rajeevcluster1_db_user:6yRfw37RbkcsCM7u@cluster1.9dm9by3.mongodb.net/?appName=Cluster1';
+  await mongoose.connect(connectionString);
+  mongoose.set('strictQuery', true);
+}
