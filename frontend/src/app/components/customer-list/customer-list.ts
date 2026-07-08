a943c6ce17fd705c535f137e1f36060045fd772a
Rajeev0011 <rajeevsinghmca01@gmail.com>
2026-07-09 00:30:21 +0530

    Changes in services modils and component module

--- frontend/src/app/components/customer-list/customer-list.ts
+++ frontend/src/app/components/customer-list/customer-list.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import { CustomerService } from '../../services/customer'
+import { error, log } from 'console';
+import { CommonModule } from '@angular/common';
+import { Customer } from '../../models/customer.model';
 @Component({
   selector: 'app-customer-list',
   imports: [
     MatButtonModule,
-    RouterModule
+    RouterModule,
+    CommonModule
   ],
   templateUrl: './customer-list.html',
   styleUrl: './customer-list.css',
 })
-export class CustomerList {}
+export class CustomerList implements OnInit{
+ 
+  //Inject customer service
+  private customerService = inject(CustomerService);
+  //customer list
+  customers: Customer[] = [];
+  ngOnInit(): void {
+    this.customerService.get().subscribe(
+      data =>{
+        this.customers = data;
+        console.log(this.customers);
+        
+      },
+      error => {
+        console.log("error:",error);
+      }
+    )
+  }
+}
--- frontend/src/app/services/customer.ts
+++ frontend/src/app/services/customer.ts
@@ -1,6 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Customer } from '../models/customer.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Customer {}
+export class CustomerService {
+  private api = "http://localhost:3001/api/customers/"
+
+  constructor(
+   private http:HttpClient
+  ){}
+
+  get():Observable<Customer[]>{
+    return this.http.get<Customer[]>(this.api);
+  }
+  getById(id: String):Observable<Customer>{
+    return this.http.get<Customer>(this.api + "/" + id);
+  }
+  post(customer:Customer):Observable<Customer>{
+    return this.http.post<Customer>(this.api,customer);
+  }
+  put(id: String,customer:Customer):Observable<Customer>{
+    return this.http.put<Customer>(this.api + "/" + id, customer);
+  }
+  delete(id: String):Observable<Customer>{
+    return this.http.delete<Customer>(this.api + "/" + id);
+  }
+}
+
